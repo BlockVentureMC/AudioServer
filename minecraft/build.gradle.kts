566b394326b36ffcd3b40d895210370687f5df66
CoasterFreakDE <mleaked@gmail.com>
2024-04-30 13:04:14 +0200

    feat: Publish

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm")
+    id("maven-publish")
 }
 
 group = "de.themeparkcraft.audioserver.minecraft"
-version = "1.0-SNAPSHOT"
+version = "0.1-SNAPSHOT"
 
 val minecraftVersion: String by project
 
@@ -11,4 +12,23 @@ dependencies {
     implementation(project(":common"))
 
     compileOnly("io.papermc.paper:paper-api:$minecraftVersion")
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/BlockVentureMC/AudioServer")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+        }
+    }
 }
